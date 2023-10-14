8e9f101c94d2533fe00e2b22c35031cdd646c778
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-10-14 12:44:10 -0700

    Added Furry Friends and a pre-packaged Model Pack

---
+++ packs/furry-friends-unfinished/main.lua
@@ -0,0 +1,34 @@
+-- name: [CS] Furry Friends [Unreleased Build]
+-- description: A character swap mod using the Character Select's API.
+
+local E_MODEL_LION = smlua_model_util_get_id("yuyake_geo")
+local E_MODEL_ANGEL = smlua_model_util_get_id("yuyake_angel_geo")
+local E_MODEL_WOLFGIRL = smlua_model_util_get_id("brianna_geo")
+local E_MODEL_DOLPHFOX = smlua_model_util_get_id("veph_geo")
+local E_MODEL_HUSKY = smlua_model_util_get_id("dirk_geo")
+local E_MODEL_LIZARD = smlua_model_util_get_id("flare_geo")
+local E_MODEL_FALCON = smlua_model_util_get_id("ryder_geo")
+local E_MODEL_WOLFBOY = smlua_model_util_get_id("bradley_geo")
+local E_MODEL_BABENOOK = smlua_model_util_get_id("mina_geo")
+local E_MODEL_PBEAR = smlua_model_util_get_id("kuma_geo")
+local E_MODEL_TOONNOOK = smlua_model_util_get_id("skipper_geo")
+
+local CT_MARIO = CT_MARIO
+local CT_LUIGI = CT_LUIGI
+local CT_TOAD = CT_TOAD
+local CT_WALUIGI = CT_WALUIGI
+local CT_WARIO = CT_WARIO
+
+if _G.charSelectExists then
+    _G.charSelect.character_add("Yuyake", nil, "AngelicMiracles", E_MODEL_LION, CT_MARIO)
+    _G.charSelect.character_add("Angel Yuyake", nil, "AngelicMiracles", E_MODEL_ANGEL, CT_MARIO)
+    _G.charSelect.character_add("Brianna", nil, "AngelicMiracles", E_MODEL_WOLFGIRL, CT_MARIO)
+    _G.charSelect.character_add("Veph", nil, "AngelicMiracles", E_MODEL_DOLPHFOX, CT_MARIO)
+    _G.charSelect.character_add("Dirk", nil, "AngelicMiracles", E_MODEL_HUSKY, CT_MARIO)
+    _G.charSelect.character_add("Flare", nil, "AngelicMiracles", E_MODEL_LIZARD, CT_MARIO)
+    _G.charSelect.character_add("Ryder", nil, "AngelicMiracles", E_MODEL_FALCON, CT_MARIO)
+    _G.charSelect.character_add("Bradly", nil, "AngelicMiracles", E_MODEL_WOLFBOY, CT_MARIO)
+    _G.charSelect.character_add("Mina", nil, "AngelicMiracles", E_MODEL_BABENOOK, CT_MARIO)
+    _G.charSelect.character_add("Kuma", nil, "AngelicMiracles", E_MODEL_PBEAR, CT_WARIO)
+    _G.charSelect.character_add("Skipper", nil, "AngelicMiracles", E_MODEL_TOONNOOK, CT_MARIO)
+end
