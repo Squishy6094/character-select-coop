7b580bb5cc1a5ab4305ba9650f4f3ec1fd193e7f
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-10-14 20:52:49 -0700

    Added Header + Locked Camera

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ local currChar = 1
 
 local E_MODEL_ARMATURE = smlua_model_util_get_id("armature_geo")
 
+local TEX_HEADER = get_texture_info("char-select-text")
+
 local characterTable = {
     [1]  = {
         name = "Default",
@@ -56,6 +58,15 @@ local function mario_update(m)
     if gPlayerSyncTable[m.playerIndex].modelId ~= nil then
         obj_set_model_extended(m.marioObj, gPlayerSyncTable[m.playerIndex].modelId)
     end
+
+    if menu then
+        camera_freeze()
+        gLakituState.pos.x = m.pos.x + 500
+        gLakituState.pos.y = m.pos.y + 300
+        gLakituState.pos.z = m.pos.z + 500
+    else
+        camera_unfreeze()
+    end
 end
 
 local function set_model(o, id)
@@ -79,17 +90,19 @@ local animTimer = 0
 local function on_hud_render()
     djui_hud_set_resolution(RESOLUTION_N64)
 
-    local width = djui_hud_get_screen_width() + 3
+    local width = djui_hud_get_screen_width() + 1.2
     local height = djui_hud_get_screen_height()
     local widthHalf = width*0.5
     local heightHalf = height*0.5
 
     if menu then
+        --Character Buttons
         djui_hud_set_color(characterTable[currChar].color.r, characterTable[currChar].color.g, characterTable[currChar].color.b, 255)
-        djui_hud_render_rect(0, 0, 150, height)
+        djui_hud_render_rect(0, 0, 130, height)
         djui_hud_set_color(0, 0, 0, 255)
-        djui_hud_render_rect(2, 2, 146, height - 4)
+        djui_hud_render_rect(2, 2, 126, height - 4)
 
+        animTimer = animTimer + 1
 
         for i = -1, 4 do
             if characterTable[i + currChar] ~= nil then
@@ -97,14 +110,38 @@ local function on_hud_render()
                 djui_hud_set_color(buttonColor.r, buttonColor.g, buttonColor.b, 255)
                 local x = 30
                 if i == 0 then x = x + math.sin(animTimer*0.05)*2.5 + 5 end
-                djui_hud_render_rect(x, (i + 2) * 30 + 10, 70, 20)
+                local y = (i + 2) * 30 + 30
+                djui_hud_render_rect(x, y, 70, 20)
                 djui_hud_set_color(0, 0, 0, 255)
-                djui_hud_render_rect(x + 1, (i + 2) * 30 + 11, 68, 18)
+                djui_hud_render_rect(x + 1, y + 1, 68, 18)
                 djui_hud_set_font(FONT_NORMAL)
                 djui_hud_set_color(buttonColor.r, buttonColor.g, buttonColor.b, 255)
-                djui_hud_print_text(characterTable[currChar + i].name, x + 5, (i + 2) * 30 + 15, 0.3)
+                djui_hud_print_text(characterTable[currChar + i].name, x + 5, y + 5, 0.3)
             end
         end
+
+        djui_hud_set_color(buttonColor.r, buttonColor.g, buttonColor.b, 255)
+        djui_hud_render_rect(10, 55, 7, 180)
+        djui_hud_set_color(0, 0, 0, 255)
+        djui_hud_render_rect(11, 56, 5, 178)
+        djui_hud_set_color(buttonColor.r, buttonColor.g, buttonColor.b, 255)
+        djui_hud_render_rect(12, 57 + 176 * ((currChar - 1) / #characterTable), 3, 176/#characterTable)
+
+        
+        --Character Description
+        djui_hud_set_color(characterTable[currChar].color.r, characterTable[currChar].color.g, characterTable[currChar].color.b, 255)
+        djui_hud_render_rect(width - 130, 0, 130, height)
+        djui_hud_set_color(0, 0, 0, 255)
+        djui_hud_render_rect(width - 128, 2, 126, height - 4)
+
+        --Character Select Header
+        djui_hud_set_color(characterTable[currChar].color.r, characterTable[currChar].color.g, characterTable[currChar].color.b, 255)
+        djui_hud_render_rect(0, 0, width, 50)
+        djui_hud_set_color(0, 0, 0, 255)
+        djui_hud_render_rect(2, 2, width - 4, 46)
+        djui_hud_set_color(255, 255, 255, 255)
+        djui_hud_render_texture(TEX_HEADER, widthHalf - 128, 10, 1, 1)
+
     end
 end
 
@@ -117,26 +154,18 @@ local function before_mario_update(m)
     if menu then
         if inputStallTimer == 0 then
             if (m.controller.buttonPressed & D_JPAD) ~= 0 then
-                prevColor = characterTable[currChar].color
-                transTimer = 1
                 currChar = currChar + 1
                 inputStallTimer = inputStallTo
             end
             if (m.controller.buttonPressed & U_JPAD) ~= 0 then
-                prevColor = characterTable[currChar].color
-                transTimer = 1
                 currChar = currChar - 1
                 inputStallTimer = inputStallTo
             end
             if m.controller.stickY < -60 then
-                prevColor = characterTable[currChar].color
-                transTimer = 1
                 currChar = currChar + 1
                 inputStallTimer = inputStallTo
             end
             if m.controller.stickY > 60 then
-                prevColor = characterTable[currChar].color
-                transTimer = 1
                 currChar = currChar - 1
                 inputStallTimer = inputStallTo
             end
