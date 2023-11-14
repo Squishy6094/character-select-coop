2c4a588f71028e0f5f061fbb54e0193b9b7c9484
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-11-14 14:00:57 -0800

    Good Morning!

--- packs/char-select-uncle-grandpa/main.lua
+++ packs/char-select-uncle-grandpa/main.lua
@@ -7,7 +7,7 @@ local E_MODEL_UNCLE_GRANDPA = smlua_model_util_get_id("uncle_grandpa_geo")
 local TEXT_MOD_NAME = "Uncle Grandpa"
 
 if _G.charSelectExists then
-    _G.charSelect.character_add("Uncle Grandpa", nil, "Garlicker", {r = 188, g = 97, b = 39}, E_MODEL_UNCLE_GRANDPA, CT_MARIO)
+    _G.charSelect.character_add("Uncle Grandpa", {"Good Morning!"}, "Garlicker", {r = 188, g = 97, b = 39}, E_MODEL_UNCLE_GRANDPA, CT_MARIO)
 else
     djui_popup_create("\\#ffffdc\\\n"..TEXT_MOD_NAME.."\nRequires the Character Select Mod\nto use as a Library!\n\nPlease turn on the Character Select Mod\nand Restart the Room!", 6)
 end
