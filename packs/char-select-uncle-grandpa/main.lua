74b4cd6376798583c715ec3174e582f536c43417
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-12-12 10:06:44 -0800

    Added Uncle Grandpa Icon (Kitkat)

--- packs/char-select-uncle-grandpa/main.lua
+++ packs/char-select-uncle-grandpa/main.lua
@@ -3,11 +3,13 @@
 
 local E_MODEL_UNCLE_GRANDPA = smlua_model_util_get_id("uncle_grandpa_geo")
 
+local TEX_UNCLE_GRANDPA = get_texture_info("uncle-grandpa-icon")
+
 -- Model Pack name used 
 local TEXT_MOD_NAME = "Uncle Grandpa"
 
 if _G.charSelectExists then
-    _G.charSelect.character_add("Uncle Grandpa", {"Good Morning!"}, "Garlicker", {r = 188, g = 97, b = 39}, E_MODEL_UNCLE_GRANDPA, CT_MARIO)
+    _G.charSelect.character_add("Uncle Grandpa", {"Good Morning!"}, "Garlicker", {r = 188, g = 97, b = 39}, E_MODEL_UNCLE_GRANDPA, CT_MARIO, TEX_UNCLE_GRANDPA)
 else
     djui_popup_create("\\#ffffdc\\\n"..TEXT_MOD_NAME.."\nRequires the Character Select Mod\nto use as a Library!\n\nPlease turn on the Character Select Mod\nand Restart the Room!", 6)
 end
