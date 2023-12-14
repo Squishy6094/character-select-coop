07c1f38582d7e1a58d06bf49b1051eaf7630588a
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2023-12-14 09:44:54 -0800

    Added (Almost) All Pizza Pack Sounds (need noise)

--- packs/char-select-pizza-tower-pack/main.lua
+++ packs/char-select-pizza-tower-pack/main.lua
@@ -1,5 +1,5 @@
 -- name: [CS] Pizizito's Pizza Tower Pack
--- description: A handful of Pizza Tower/Sugary Spire Characters in Super Mario 64!\n\nModels by: Pizizito\n\n\\#ff7777\\This Mod Requires the Character Select Mod\nto use as a Library!
+-- description: A handful of Pizza Tower/Sugary Spire Characters in Super Mario 64!\n\nModels by: Pizizito\nHUD Icons by: KitKat\nSounds by: Tour de Pizza\n\n\\#ff7777\\This Mod Requires the Character Select Mod\nto use as a Library!
 
 local E_MODEL_PEPPERMAN = smlua_model_util_get_id("pepperman_geo")
 local E_MODEL_VIGILANTE = smlua_model_util_get_id("vigilante_geo")
@@ -16,15 +16,51 @@ local TEX_PIZZELLE = get_texture_info("pizzelle-icon")
 local TEX_PIZZANO = get_texture_info("pizzano-icon")
 
 local VOICETABLE_PEPPERMAN = {
+    [CHAR_SOUND_ATTACKED] = 'PeppermanScared.ogg',
+    [CHAR_SOUND_HAHA] = 'PeppermanLaugh.ogg',
+    [CHAR_SOUND_HAHA_2] = 'PeppermanLaugh.ogg',
+    [CHAR_SOUND_HERE_WE_GO] = 'PeppermanLaugh.ogg',
+    [CHAR_SOUND_MAMA_MIA] = 'PeppermanScared.ogg',
+    [CHAR_SOUND_OKEY_DOKEY] = 'PeppermanSnicker.ogg',
+    [CHAR_SOUND_ON_FIRE] = 'PeppermanScared.ogg',
+    [CHAR_SOUND_OOOF] = 'PeppermanScared.ogg',
+    [CHAR_SOUND_OOOF2] = 'PeppermanScared.ogg',
+    [CHAR_SOUND_SO_LONGA_BOWSER] = 'PeppermanLaugh.ogg',
+    [CHAR_SOUND_TWIRL_BOUNCE] = 'PeppermanSnicker.ogg',
+    [CHAR_SOUND_WHOA] = 'PeppermanScared.ogg',
+    [CHAR_SOUND_YAHOO] = 'PeppermanSnicker.ogg',
+    [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = 'PeppermanSnicker.ogg',
+}
+
+local VOICETABLE_VIGILANTE = {
+    [CHAR_SOUND_ATTACKED] = {'vigi1.ogg', 'vigi2.ogg',},
+    [CHAR_SOUND_DYING] = 'vigi2.ogg',
+    [CHAR_SOUND_GROUND_POUND_WAH] = 'vigi3.ogg',
+    [CHAR_SOUND_HAHA] = {'vigi1.ogg', 'vigi2.ogg', 'vigi3.ogg'},
+    [CHAR_SOUND_HAHA_2] = {'vigi1.ogg', 'vigi2.ogg', 'vigi3.ogg'},
+    [CHAR_SOUND_HERE_WE_GO] = {'vigi1.ogg', 'vigi2.ogg', 'vigi3.ogg'},
+    [CHAR_SOUND_MAMA_MIA] = 'vigi2.ogg',
+    [CHAR_SOUND_OKEY_DOKEY] = 'vigi3.ogg',
+    [CHAR_SOUND_OOOF] = 'vigi2.ogg',
+    [CHAR_SOUND_OOOF2] = 'vigi2.ogg',
+    [CHAR_SOUND_SO_LONGA_BOWSER] = 'vigi1.ogg',
+    [CHAR_SOUND_TWIRL_BOUNCE] = 'vigi3.ogg',
+    [CHAR_SOUND_WAH2] = 'vigi1.ogg',
+    [CHAR_SOUND_WHOA] = {'vigi1.ogg', 'vigi2.ogg',},
+    [CHAR_SOUND_YAHOO] = 'vigi1.ogg',
+    [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = 'vigi3.ogg',
+}
+
+--[[local VOICETABLE_NOISE = {
     [CHAR_SOUND_ATTACKED] = nil,
     [CHAR_SOUND_DOH] = nil,
     [CHAR_SOUND_DROWNING] = nil,
     [CHAR_SOUND_DYING] = nil,
     [CHAR_SOUND_GROUND_POUND_WAH] = nil,
-    [CHAR_SOUND_HAHA] = 'PeppermanLaugh.ogg',
-    [CHAR_SOUND_HAHA_2] = 'PeppermanLaugh.ogg',
-    [CHAR_SOUND_HERE_WE_GO] = 'PeppermanLaugh.ogg',
-    [CHAR_SOUND_HOOHOO] = 'PeppermanSnicker.ogg',
+    [CHAR_SOUND_HAHA] = nil,
+    [CHAR_SOUND_HAHA_2] = nil,
+    [CHAR_SOUND_HERE_WE_GO] = nil,
+    [CHAR_SOUND_HOOHOO] = nil,
     [CHAR_SOUND_MAMA_MIA] = nil,
     [CHAR_SOUND_OKEY_DOKEY] = nil,
     [CHAR_SOUND_ON_FIRE] = nil,
@@ -42,7 +78,33 @@ local VOICETABLE_PEPPERMAN = {
     [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = nil,
     [CHAR_SOUND_YAH_WAH_HOO] = nil,
     [CHAR_SOUND_YAWNING] = nil,
+}]]
+
+local VOICETABLE_FAKE_PEPPINO = {
+    [CHAR_SOUND_ATTACKED] = 'fakepepnegative1.ogg',
+    [CHAR_SOUND_GROUND_POUND_WAH] = 'fakepeppositive2.ogg',
+    [CHAR_SOUND_HAHA] = {'fakepeppositive1.ogg', 'fakepeppositive2.ogg', 'fakepeppositive3.ogg'},
+    [CHAR_SOUND_HAHA_2] = {'fakepeppositive1.ogg', 'fakepeppositive2.ogg', 'fakepeppositive3.ogg'},
+    [CHAR_SOUND_HERE_WE_GO] = {'fakepeppositive1.ogg', 'fakepeppositive2.ogg', 'fakepeppositive3.ogg'},
+    [CHAR_SOUND_HOOHOO] = 'fakepeppositive2.ogg',
+    [CHAR_SOUND_MAMA_MIA] = 'fakepepnegative2.ogg',
+    [CHAR_SOUND_OKEY_DOKEY] = {'fakepeppositive1.ogg', 'fakepeppositive2.ogg', 'fakepeppositive3.ogg'},
+    [CHAR_SOUND_ON_FIRE] = 'fakepepscream.ogg',
+    [CHAR_SOUND_OOOF] = 'fakepepnegative1.ogg',
+    [CHAR_SOUND_OOOF2] = 'fakepepnegative1.ogg',
+    [CHAR_SOUND_SO_LONGA_BOWSER] = 'fakepepnegitive2.ogg',
+    [CHAR_SOUND_TWIRL_BOUNCE] = {'fakepeppositive1.ogg', 'fakepeppositive2.ogg', 'fakepeppositive3.ogg'},
+    [CHAR_SOUND_WAAAOOOW] = 'fakepepscream.ogg',
+    [CHAR_SOUND_WHOA] = 'fakepepnegative2.ogg',
+    [CHAR_SOUND_YAHOO] = 'fakepeppositive1.ogg',
+    [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = {'fakepeppositive1.ogg', 'fakepeppositive2.ogg', 'fakepeppositive3.ogg'},
 }
+
+local VOICETABLE_PIZZELLE = {
+    [CHAR_SOUND_ON_FIRE] = 'pizzellefireass.ogg', -- Literally her only sfx
+}
+
+local VOICETABLE_PIZZANO = {nil} -- The Italian man don't talk anymore :<
 
 local TEXT_MOD_NAME = "Pizizito's Pizza Tower Pack"
 
@@ -55,11 +117,23 @@ if _G.charSelectExists then
     _G.charSelect.character_add("Pizzano", {"An immature, eccentric, and", "occasionally rather violent", "TV network host of", "The Sugary Spire"}, "Pizizito", {r = 75, g = 160, b = 253}, E_MODEL_PIZZANO, CT_WARIO, TEX_PIZZANO)
 
     _G.charSelect.character_add_voice(E_MODEL_PEPPERMAN, VOICETABLE_PEPPERMAN)
+    _G.charSelect.character_add_voice(E_MODEL_VIGILANTE, VOICETABLE_VIGILANTE)
+    _G.charSelect.character_add_voice(E_MODEL_FAKE_PEPPINO, VOICETABLE_FAKE_PEPPINO)
+    _G.charSelect.character_add_voice(E_MODEL_PIZZELLE, VOICETABLE_PIZZELLE)
+    _G.charSelect.character_add_voice(E_MODEL_PIZZANO, VOICETABLE_PIZZANO)
     hook_event(HOOK_CHARACTER_SOUND, function (m, sound)
         if _G.charSelect.character_get_voice(m) == VOICETABLE_PEPPERMAN then return _G.charSelect.voice.sound(m, sound) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_VIGILANTE then return _G.charSelect.voice.sound(m, sound) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_FAKE_PEPPINO then return _G.charSelect.voice.sound(m, sound) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_PIZZELLE then return _G.charSelect.voice.sound(m, sound) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_PIZZANO then return _G.charSelect.voice.sound(m, sound) end
     end)
     hook_event(HOOK_MARIO_UPDATE, function (m)
         if _G.charSelect.character_get_voice(m) == VOICETABLE_PEPPERMAN then return _G.charSelect.voice.snore(m) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_VIGILANTE then return _G.charSelect.voice.snore(m) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_FAKE_PEPPINO then return _G.charSelect.voice.snore(m) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_PIZZELLE then return _G.charSelect.voice.snore(m) end
+        if _G.charSelect.character_get_voice(m) == VOICETABLE_PIZZANO then return _G.charSelect.voice.snore(m) end
     end)
 else
     djui_popup_create("\\#ffffdc\\\n"..TEXT_MOD_NAME.."\nRequires the Character Select Mod\nto use as a Library!\n\nPlease turn on the Character Select Mod\nand Restart the Room!", 6)
