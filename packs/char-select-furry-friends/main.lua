08dca609f122f81d2d6f9db3a5729e23ca5fef62
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2024-01-16 13:15:43 -0800

    Removed Angel Yuyake from Furry Friends

--- packs/char-select-furry-friends/main.lua
+++ packs/char-select-furry-friends/main.lua
@@ -2,7 +2,6 @@
 -- description: Models of project owner Yuyake and his friends.\n\nModels by: AngelicMiracles\n\n\\#ff7777\\This Pack requires Character Select\nto use as a Library!
 
 local E_MODEL_LION = smlua_model_util_get_id("yuyake_geo")
-local E_MODEL_ANGEL = smlua_model_util_get_id("yuyake_angel_geo")
 local E_MODEL_WOLFGIRL = smlua_model_util_get_id("brianna_geo")
 local E_MODEL_DOLPHFOX = smlua_model_util_get_id("veph_geo")
 local E_MODEL_HUSKY = smlua_model_util_get_id("dirk_geo")
@@ -17,7 +16,6 @@ if _G.charSelectExists then
     if not _G.charSelect.character_get_number_from_string("Yuyake") then
         _G.charSelect.character_add("Yuyake", {" A lion warrior who joined", "the military to fight off", "against the conquerer known", "as Kaizer. Lives in the", "flying islands known as", "Sky Archipelago."}, "AngelicMiracles", {r = 232, g = 54, b = 30}, E_MODEL_LION, CT_MARIO)
     end
-    _G.charSelect.character_add("Angel Yuyake", {" Yuyake's true final form", "that shifts colors depending", "on his emotions."}, "AngelicMiracles", {r = 46, g = 220, b = 225}, E_MODEL_ANGEL, CT_MARIO)
     _G.charSelect.character_add("Brianna", {" A wolf woman who is a", "military commander leading", "the mission on defeating.", "Kaizer."}, "AngelicMiracles", {r = 18, g = 184, b = 71}, E_MODEL_WOLFGIRL, CT_LUIGI)
     _G.charSelect.character_add("Veph", {" A dolphin-fox who hangs", "around with Sonic and co."}, "AngelicMiracles", {r = 242, g = 36, b = 74}, E_MODEL_DOLPHFOX, CT_MARIO)
     _G.charSelect.character_add("Dirk", {"A husky with the abilities of ice!"}, "AngelicMiracles", {r = 51, g = 38, b = 135}, E_MODEL_HUSKY, CT_MARIO)
