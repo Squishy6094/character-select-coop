3bc3a2cd80c92f205f09f712788e3d3a51f802ce
Squishy <74333752+SQUISHY6094@users.noreply.github.com>
2024-12-03 08:10:46 -0800

    fixed In-Dev error, set In-Dev to false

--- a-utils.lua
+++ a-utils.lua
@@ -10,8 +10,8 @@ end
 MOD_VERSION_API = 1
 MOD_VERSION_MAJOR = 11
 MOD_VERSION_MINOR = 0
-MOD_VERSION_INDEV = true
-MOD_VERSION_STRING = tostring(MOD_VERSION_API) .. "." .. tostring(MOD_VERSION_MAJOR) .. (MOD_VERSION_MINOR > 0 and ("." .. tostring(MOD_VERSION_MINOR)) or "") .. (MOD_VERSION_INDEV and (" (In-Dev)" or ""))
+MOD_VERSION_INDEV = false
+MOD_VERSION_STRING = tostring(MOD_VERSION_API) .. "." .. tostring(MOD_VERSION_MAJOR) .. (MOD_VERSION_MINOR > 0 and ("." .. tostring(MOD_VERSION_MINOR)) or "") .. (MOD_VERSION_INDEV and " (In-Dev)" or "")
 
 ommActive = false
 for i in pairs(gActiveMods) do
