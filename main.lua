8682fbd21c2b4928441c9f3617c32b0f967191bf
Squishy <squishy6094@gmail.com>
2026-04-03 22:06:21 -0700

    Properly fix optimization with model swaps

--- main.lua
+++ main.lua
@@ -1335,38 +1335,21 @@ function set_model(o, model, extendedModel, charNum)
             end
         end
     else
-        local model = o.unused1
-        if characterTable[charNum].replaceModels.bhv ~= nil then -- Other Custom Behaviors
-            local currReplace = characterTable[charNum].replaceModels.bhv[get_id_from_behavior(o.behavior)]
-            if currReplace ~= nil then
-                model = run_func_or_get_var(currReplace, o, o.unused1) or o.unused1
-                if not visualToggle then
-                    model = o.unused1
-                end
-            else
-                if o.unused1 ~= extendedModel then
-                    o.unused1 = extendedModel
-                end
+        local replaceBhv = characterTable[charNum].replaceModels.bhv[get_id_from_behavior(o.behavior)]
+        local replaceModel = characterTable[charNum].replaceModels.model[extendedModel]
+        local currReplace = replaceBhv or replaceModel
+        if currReplace then -- Other Custom Behaviors
+            local model = run_func_or_get_var(currReplace, o, o.unused1) or o.unused1
+            if not visualToggle then
+                model = o.unused1
             end
-        end
-
-        if characterTable[charNum].replaceModels.model ~= nil then -- Other Custom Models
-            local currReplace = characterTable[charNum].replaceModels.model[extendedModel]
-            if currReplace ~= nil then
-                model = run_func_or_get_var(currReplace, o, o.unused1) or o.unused1
-                if not visualToggle then
-                    model = o.unused1
-                end
-            else
-                if o.unused1 ~= extendedModel then
-                    o.unused1 = extendedModel
-                end
+            
+            if obj_has_model_extended(o, model) == 0 then
+                settingModel = true
+                obj_set_model_extended(o, model)
             end
-        end
-
-        if obj_has_model_extended(o, model) == 0 then
-            settingModel = true
-            obj_set_model_extended(o, model)
+        elseif o.unused1 ~= extendedModel then
+            o.unused1 = extendedModel
         end
     end
 end
